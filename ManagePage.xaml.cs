a5fe93dffcdd56d18cfecf6d72edc987872ca9b4
znc15 <jimmybandass21@gmail.com>
2024-06-07 12:38:30 +0800

    修复没有对齐按钮的问题

--- ManagePage.xaml.cs
+++ ManagePage.xaml.cs
@@ -33,11 +33,11 @@ namespace MinecraftServerLauncher
             {
                 Children =
                 {
-                    CreateTextBoxWithFilePicker("服务器名称", "输入服务器名称", false, "例子: 我的服务器"),
+                    CreateTextBox("服务器名称", "输入服务器名称", "例子: 我的服务器"),
                     CreateTextBoxWithFilePicker("服务端核心位置", "选择服务端核心位置", true, "例子: C:\\Minecraft\\server.jar"),
-                    CreateTextBoxWithFilePicker("启动参数", "输入启动参数", false, "例子: -Xmx1024M -Xms1024M -jar minecraft_server.jar nogui"),
+                    CreateTextBox("启动参数", "输入启动参数", "例子: -Xmx1024M -Xms1024M -jar minecraft_server.jar nogui"),
                     CreateTextBoxWithFilePicker("Java地址", "选择Java地址", true, "例子: C:\\Program Files\\Java\\jdk-16\\bin\\java.exe"),
-                    CreateTextBoxWithFilePicker("服务器文件地址", "选择服务器文件地址", true, "例子: C:\\Minecraft\\server")
+                    CreateTextBoxWithFilePicker("服务器文件地址", "选择服务器文件地址", true, "例子: C:\\Minecraft\\server", isFolderPicker: true)
                 }
             };
 
@@ -48,7 +48,7 @@ namespace MinecraftServerLauncher
                 CloseButtonText = "取消",
                 DefaultButton = ContentDialogButton.Primary,
                 XamlRoot = this.XamlRoot,
-                Width = 500, // 设置对话框宽度
+                Width = 1000, // 设置对话框宽度
                 Content = dialogContent
             };
 
@@ -57,11 +57,11 @@ namespace MinecraftServerLauncher
 
             if (result == ContentDialogResult.Primary)
             {
-                var serverNameBox = (TextBox)((StackPanel)dialogContent.Children[0]).Children[0];
-                var serverCorePathBox = (TextBox)((StackPanel)dialogContent.Children[1]).Children[0];
-                var startArgumentsBox = (TextBox)((StackPanel)dialogContent.Children[2]).Children[0];
-                var javaPathBox = (TextBox)((StackPanel)dialogContent.Children[3]).Children[0];
-                var serverFilePathBox = (TextBox)((StackPanel)dialogContent.Children[4]).Children[0];
+                var serverNameBox = (TextBox)((Grid)((StackPanel)dialogContent.Children[0]).Children[0]).Children[0];
+                var serverCorePathBox = (TextBox)((Grid)((StackPanel)dialogContent.Children[1]).Children[0]).Children[0];
+                var startArgumentsBox = (TextBox)((Grid)((StackPanel)dialogContent.Children[2]).Children[0]).Children[0];
+                var javaPathBox = (TextBox)((Grid)((StackPanel)dialogContent.Children[3]).Children[0]).Children[0];
+                var serverFilePathBox = (TextBox)((Grid)((StackPanel)dialogContent.Children[4]).Children[0]).Children[0];
 
                 var server = new MinecraftServer
                 {
@@ -177,46 +177,83 @@ namespace MinecraftServerLauncher
             EmptyMessage.Visibility = Servers.Count == 0 ? Visibility.Visible : Visibility.Collapsed;
         }
 
-        private StackPanel CreateTextBoxWithFilePicker(string header, string placeholderText, bool isFilePicker, string exampleText)
+        private StackPanel CreateTextBox(string header, string placeholderText, string exampleText)
         {
-            var stackPanel = new StackPanel { Orientation = Orientation.Vertical, Margin = new Thickness(0, 20, 0, 0) };
-            var textBox = new TextBox { Header = header, PlaceholderText = placeholderText, Name = header, Width = 480 };
+            var stackPanel = new StackPanel { Orientation = Orientation.Vertical, Margin = new Thickness(0, 27, 0, 0) };
 
-            textBox.Tapped += async (sender, e) =>
+            var grid = new Grid();
+            grid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+
+            var textBox = new TextBox { Header = header, PlaceholderText = placeholderText, Name = header };
+            Grid.SetColumn(textBox, 0);
+            grid.Children.Add(textBox);
+
+            stackPanel.Children.Add(grid);
+
+            var exampleTextBlock = new TextBlock
+            {
+                Text = exampleText,
+                Foreground = new SolidColorBrush(Microsoft.UI.Colors.Gray),
+                Margin = new Thickness(0, 5, 0, 0)
+            };
+            stackPanel.Children.Add(exampleTextBlock);
+
+            return stackPanel;
+        }
+
+        private StackPanel CreateTextBoxWithFilePicker(string header, string placeholderText, bool isFilePicker, string exampleText, bool isFolderPicker = false)
+        {
+            var stackPanel = new StackPanel { Orientation = Orientation.Vertical, Margin = new Thickness(0, 27, 0, 0) };
+
+            var grid = new Grid();
+            grid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+            grid.ColumnDefinitions.Add(new ColumnDefinition { Width = GridLength.Auto });
+
+            var textBox = new TextBox { Header = header, PlaceholderText = placeholderText, Name = header };
+            Grid.SetColumn(textBox, 0);
+            grid.Children.Add(textBox);
+
+            var button = new Button { Content = "浏览", Margin = new Thickness(12, 27, 0, 0), Width = 60, VerticalAlignment = VerticalAlignment.Center };
+            button.Click += async (sender, e) =>
             {
                 if (isFilePicker)
                 {
-                    var picker = new FileOpenPicker();
-                    picker.SuggestedStartLocation = PickerLocationId.Desktop;
-                    picker.FileTypeFilter.Add("*");
-
-                    var hwnd = WinRT.Interop.WindowNative.GetWindowHandle(App.MainWindow);
-                    WinRT.Interop.InitializeWithWindow.Initialize(picker, hwnd);
-
-                    StorageFile file = await picker.PickSingleFileAsync();
-                    if (file != null)
+                    if (isFolderPicker)
                     {
-                        textBox.Text = file.Path;
+                        var picker = new FolderPicker();
+                        picker.SuggestedStartLocation = PickerLocationId.Desktop;
+                        picker.FileTypeFilter.Add("*");
+
+                        var hwnd = WinRT.Interop.WindowNative.GetWindowHandle(App.MainWindow);
+                        WinRT.Interop.InitializeWithWindow.Initialize(picker, hwnd);
+
+                        StorageFolder folder = await picker.PickSingleFolderAsync();
+                        if (folder != null)
+                        {
+                            textBox.Text = folder.Path;
+                        }
                     }
-                }
-                else
-                {
-                    var picker = new FolderPicker();
-                    picker.SuggestedStartLocation = PickerLocationId.Desktop;
-                    picker.FileTypeFilter.Add("*");
-
-                    var hwnd = WinRT.Interop.WindowNative.GetWindowHandle(App.MainWindow);
-                    WinRT.Interop.InitializeWithWindow.Initialize(picker, hwnd);
-
-                    StorageFolder folder = await picker.PickSingleFolderAsync();
-                    if (folder != null)
+                    else
                     {
-                        textBox.Text = folder.Path;
+                        var picker = new FileOpenPicker();
+                        picker.SuggestedStartLocation = PickerLocationId.Desktop;
+                        picker.FileTypeFilter.Add("*");
+
+                        var hwnd = WinRT.Interop.WindowNative.GetWindowHandle(App.MainWindow);
+                        WinRT.Interop.InitializeWithWindow.Initialize(picker, hwnd);
+
+                        StorageFile file = await picker.PickSingleFileAsync();
+                        if (file != null)
+                        {
+                            textBox.Text = file.Path;
+                        }
                     }
                 }
             };
+            Grid.SetColumn(button, 1);
+            grid.Children.Add(button);
 
-            stackPanel.Children.Add(textBox);
+            stackPanel.Children.Add(grid);
 
             var exampleTextBlock = new TextBlock
             {
